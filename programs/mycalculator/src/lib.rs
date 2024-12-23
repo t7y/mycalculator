022f1cbfbfb7debbdaeaf009533351b65ca6f9f5
Jerry Tian <jerry@alchemy.com>
2024-12-23 15:14:40 -0800

    addition

--- programs/mycalculator/src/lib.rs
+++ programs/mycalculator/src/lib.rs
@@ -11,6 +11,12 @@ pub mod mycalculator {
         calculator.greeting = init_message;
         Ok(())
     }
+
+    pub fn add(ctx: Context<Addition>, num1: i64, num2: i64) -> Result<()> {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 + num2;
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
@@ -21,6 +27,12 @@ pub struct Create<'info>{
     #[account(mut)] // user is mutable
     pub user: Signer<'info>,
     pub system_program: Program<'info, System>
+}
+
+#[derive(Accounts)]
+pub struct Addition<'info>{
+    #[account(mut)]
+    pub calculator: Account<'info, Calculator>
 }
 
 #[account]
--- tests/mycalculator.ts
+++ tests/mycalculator.ts
@@ -20,4 +20,14 @@ describe('mycalculator', () => {
         const account = await program.account.calculator.fetch(calculator.publicKey)
         assert.ok(account.greeting === "Welcome to Solana")
     })
+
+    it('Adds two numbers', async() => {
+        await program.rpc.add(new anchor.BN(2), new anchor.BN(3), {
+            accounts: {
+                calculator: calculator.publicKey
+            }
+        })
+        const account = await program.account.calculator.fetch(calculator.publicKey)
+        assert.ok(account.result.eq(new anchor.BN(5)))
+    })
 })
